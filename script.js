653486bbaab61890d524a8f28a9dd2e88dfd21c5
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-27 10:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,10 @@
 function volume_sphere(event) {
     //Write your code here
     const radius = parseFloat(document.getElementById("radius").value);
+	event.preventDefault();
     const volumeField = document.getElementById("volume");
-    // if (isNaN(radius)) {
-    //     volumeField.value = NaN
-    //     return; // Exit the function if the input is invalid
-    // }
-
-    if(radius != Number) volumeField.value = NaN;
     const volumeOfSphere = (4/3) * Math.PI * Math.pow(radius,3);
     volumeField.value = volumeOfSphere.toFixed(4);
-    // event.preventDefault();
 } 
 //form is getting submitted to default(same page/same url) and automatically gets refreshed, so to stop this we use
 //event.preventDefault() 
